a56127430e15ce9e616b1e2fd45c191a192a3ae7
bkadkaltaradata1-arch <bkadkaltaradata1@gmail.com>
2025-09-27 12:33:19 +0800

    Update gagfreeku2.lua

--- gagfreeku2.lua
+++ gagfreeku2.lua
@@ -1,4 +1,4 @@
--- LocalScript  sadsa
+-- LocalScript  sadsa1
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 local UserInputService = game:GetService("UserInputService")

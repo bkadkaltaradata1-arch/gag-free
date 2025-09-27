b696ff15c9454c37a875c81ab728a8b49bd0c47d
bkadkaltaradata1-arch <bkadkaltaradata1@gmail.com>
2025-09-27 12:38:59 +0800

    Update gagfreeku2.lua

--- gagfreeku2.lua
+++ gagfreeku2.lua
@@ -1,4 +1,4 @@
--- Grow A Garden Auto Farm Scripta
+-- Grow A Garden Auto
 local Workspace = game:GetService("Workspace")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
